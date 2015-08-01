3995ef3c763c296cf9bdcda7bbaa31a808fb845a
luisolivos <pp_its@hotmail.com>
2015-08-01 12:29:33 -0500

    q15
    15

--- js/API/connection.js
+++ js/API/connection.js
@@ -1,7 +1,7 @@
 //connection.js
 var conex = {
 	isConnected: function(){
-		if(navigator.connection.type != Connection.NONE)
+		if(navigator.connection.type == Connection.WIFI)
 			return true;
 		else
 			return false;
